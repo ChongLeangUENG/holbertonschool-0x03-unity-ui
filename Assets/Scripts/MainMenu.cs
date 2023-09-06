806d3de3aa02a0bde6b5aaa911914b64e9260119
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-06 10:42:59 +0200

    Add script for quit game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,4 +8,13 @@ public class MainMenu : MonoBehaviour
         // Load the maze scene
         SceneManager.LoadScene("maze");
     }
+
+    public void QuitMaze()
+    {
+        // Display a debug message in the console
+        Debug.Log("Quit Game");
+
+        // Quit the application (works in standalone builds)
+        Application.Quit();
+    }
 }
