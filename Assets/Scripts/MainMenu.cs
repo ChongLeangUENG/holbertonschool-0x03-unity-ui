2d887aa6ba207a82ac23ece6c419ec728c4ac3b5
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-06 11:17:13 +0200

    Add colorblind mode for option

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            // Change the trap material color to orange
+            trapMat.color = new Color32(255, 112, 0, 255);
+
+            // Change the goal material color to blue
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            // Reset trap and goal materials to their default colors
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         // Load the maze scene
         SceneManager.LoadScene("maze");
     }
